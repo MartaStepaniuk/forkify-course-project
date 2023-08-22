757b97c4015e177e9b88acdbbd5a91a392af3df9
MartaStepaniuk <martastepaniuk52@gmail.com>
2023-08-22 19:07:42 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '0bb4f58b-6069-4a4e-b970-7127777ebcc2';
+export const MODAL_CLOSE_SEC = 2.5;
